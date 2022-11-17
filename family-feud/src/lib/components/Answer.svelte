e3b3a97b53287abe7193a66c7604a2e400687208
Gabriel Britain <quontas@gmail.com>
2022-11-16 21:44:03 -0800

    Better CSS animations, need to integrate with Svelte

--- family-feud/src/lib/components/Answer.svelte
+++ family-feud/src/lib/components/Answer.svelte
@@ -1,47 +1,64 @@
 <script lang="ts">
 	export let revealed = false;
 	export let rank: number;
-	export let answer: string = "";
+	export let answer: string = 'flippitygibberty';
 	export let surveyCount: number = 0;
 </script>
 
-<div class="answer" class:revealed on:click={() => revealed = !revealed}>
-	<div class="face front">{rank}</div>
-	<div class="face top">
-		<span>{answer}</span>
-		<span>{surveyCount}</span>
+<div class="flip-card">
+	<div class="flip-card-inner">
+		<div class="flip-card-front">
+			<p>{rank}</p>
+		</div>
+		<div class="flip-card-back">
+			<span>{answer}</span>
+			<span>{surveyCount}</span>
+		</div>
 	</div>
 </div>
 
 <style>
-	.answer {
-        width: 100%;
-        height: 100%;
-        padding: 32px;
-		transform-style: preserve-3d;
-        transition: all .3s ease-in;
-	}
-	.face {
-		display: flex;
-		align-items: center;
-		justify-content: center;
+	.flip-card {
+		border-style: hidden;
+		background-color: transparent;
 		width: 100%;
-		height: 100%;
-		position: absolute;
-		backface-visibility: inherit;
-		font-size: 60px;
-		color: #000;
-	}
-	.front {
-		background: rgba(90, 90, 90, 1);
-		transform: translateZ(50px);
-	}
-	.top {
-		background: rgba(210, 210, 0, 1);
-		transform: rotateX(90deg) translateZ(50px);
+		height: 4rem;
+		perspective: 1000px;
+		vertical-align: middle;
+		font-size: x-large;
+		margin-bottom: 8px;
 	}
 
-    .revealed {
-        transform: rotate3d(1, 0, 0, -90deg);
-    }
+	.flip-card-inner {
+		position: relative;
+		width: 100%;
+		height: 100%;
+		text-align: center;
+		transition: transform 0.6s;
+		transform-style: preserve-3d;
+		backface-visibility: hidden;
+	}
+
+	.flip-card:hover .flip-card-inner {
+		transform: rotateX(180deg);
+	}
+
+	.flip-card-front,
+	.flip-card-back {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		backface-visibility: hidden;
+	}
+
+	.flip-card-front {
+		background-color: #bbb;
+		color: black;
+	}
+
+	.flip-card-back {
+		background-color: #222e36ef;
+		color: white;
+		transform: rotateX(180deg);
+	}
 </style>
--- family-feud/src/routes/+page.svelte
+++ family-feud/src/routes/+page.svelte
@@ -2,26 +2,25 @@
 	import Answer from '$lib/components/Answer.svelte';
 </script>
 
-<div class="container">
-	<div class="answers">
-		<Answer rank={1} answer={"Tommy Gabbagool"} surveyCount={3} />
+<div class="columns">
+	<div>
+		<Answer rank={1} answer={"Flippers"} surveyCount={0} />
 		<Answer rank={2} />
 		<Answer rank={3} />
-        <Answer rank={4} answer={"Tommy Gabbagool"} surveyCount={3} />
+		<Answer rank={4} />
+		<Answer rank={5} />
+	</div>
+	<div>
+		<Answer rank={6} />
+		<Answer rank={7} />
+		<Answer rank={8} />
+		<Answer rank={9} />
+		<Answer rank={10} />
 	</div>
 </div>
 
 <style>
-	.container {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-        height: 100vh;
-	}
-	.answers {
-		display: grid;
-		row-gap: 8rem;
-        width: 100%;
+	.columns {
+		column-count: 2;
 	}
 </style>
