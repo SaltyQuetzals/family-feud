f4028ac162df546bfb23079387e9edb945d2998f
Gabriel Britain <quontas@gmail.com>
2022-11-16 17:47:19 -0800

    Init commit

---
+++ family-feud/src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import Answer from '$lib/components/Answer.svelte';
+</script>
+
+<div class="container">
+	<div class="answers">
+		<Answer rank={1} answer={"Tommy Gabbagool"} surveyCount={3} />
+		<Answer rank={2} />
+		<Answer rank={3} />
+        <Answer rank={4} answer={"Tommy Gabbagool"} surveyCount={3} />
+	</div>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+        height: 100vh;
+	}
+	.answers {
+		display: grid;
+		row-gap: 8rem;
+        width: 100%;
+	}
+</style>
