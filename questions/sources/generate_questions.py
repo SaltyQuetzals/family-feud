dde6309413fc2e7d65fc2a04609a1df4e1d638cd
Gabriel Britain <gbritain@microsoft.com>
2024-01-31 14:27:28 -0600

    TRY/CATCH

--- questions/sources/generate_questions.py
+++ questions/sources/generate_questions.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import re
 import glob
 
 for filename in glob.iglob('*.csv'):
@@ -8,15 +9,20 @@ for filename in glob.iglob('*.csv'):
         next(reader)
         for question, *answer_list in reader:
             print(question)
-            answers = []
-            for text, survey_count in zip(answer_list[::2], answer_list[1::2]):
-                answers.append({"text": text, "surveyCount": int(survey_count)})
-            while len(answers) < 8:
-                answers.append(None)
-            formatted = {
-                "text": question,
-                "answers": answers
-            }
-            escaped = question.replace(' ', '_').replace(',', '_').replace('?', '_')
-            with open(f"../{escaped}.json", "w+") as output_f:
-                json.dump(formatted, output_f)
+            try:
+                answers = []
+                for text, survey_count in zip(answer_list[::2], answer_list[1::2]):
+                    answers.append({"text": text, "surveyCount": int(survey_count)})
+                while len(answers) < 8:
+                    answers.append(None)
+                formatted = {
+                    "text": question,
+                    "answers": answers
+                }
+
+                
+                escaped = re.sub('[^a-zA-Z0-9]+', "-", question)
+                with open(f"../{escaped}.json", "w+") as output_f:
+                    json.dump(formatted, output_f)
+            except Exception as e:
+                continue
